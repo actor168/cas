4ca5c7f0855365131ab897a016c7d1b135f6770e
hdeadman <hal.deadman@gmail.com>
2019-03-17 01:27:01 -0400

    add tests to test suite

--- support/cas-server-support-ldap/src/test/java/org/apereo/cas/AllLdapTestsSuite.java
+++ support/cas-server-support-ldap/src/test/java/org/apereo/cas/AllLdapTestsSuite.java
@@ -1,5 +1,8 @@
 package org.apereo.cas;
 
+import org.apereo.cas.authentication.ActiveDirectoryBindDnLdapAuthenticationHandlerTests;
+import org.apereo.cas.authentication.ActiveDirectoryBindDnSSLLdapAuthenticationHandlerTests;
+import org.apereo.cas.authentication.ActiveDirectoryLdapAuthenticationHandlerTests;
 import org.apereo.cas.authentication.AuthenticatedLdapAuthenticationHandlerTests;
 import org.apereo.cas.authentication.DirectLdapAuthenticationHandlerTests;
 import org.apereo.cas.authentication.LdapPasswordSynchronizationAuthenticationPostProcessorTests;
@@ -14,6 +17,9 @@ import org.junit.platform.suite.api.SelectClasses;
  * @since 4.1.0
  */
 @SelectClasses({
+    ActiveDirectoryBindDnLdapAuthenticationHandlerTests.class,
+    ActiveDirectoryBindDnSSLLdapAuthenticationHandlerTests.class,
+    ActiveDirectoryLdapAuthenticationHandlerTests.class,
     AuthenticatedLdapAuthenticationHandlerTests.class,
     PersonDirectoryPrincipalResolverLdaptiveTests.class,
     DirectLdapAuthenticationHandlerTests.class,
